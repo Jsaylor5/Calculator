7e7eaf449b3f9385955930b1326235f6d3d8b0af
jsaylor5 <saylorj@live.com>
2021-11-03 18:26:25 -0400

    Added negative and percent buttons

--- calc.js
+++ calc.js
@@ -8,16 +8,28 @@ const divide = (a,b) => a/b;
 
 const operate = (func, num1, num2) => func(num1, num2)
 
-let displayValue =[]
+let displayValue = []
 
-const calculatorScreen = document.querySelector('#calculatorScreen')
+let calculatorScreen = document.querySelector('#calculatorScreen')
 
-const calcButtons = document.getElementById('calcButtons')
+const calcButtons = document.querySelector('#calcButtons')
 calcButtons.addEventListener('click', event => {
-    if (event.target.className === 'numButton') {
-        console.log(event.target.innerHTML);
-        document.getElementById("calculatorScreen").value = displayValue
-        displayValue += event.target.innerHTML
+    if (event.target.className === 'numButton' && displayValue.length <= 7) {
+        displayValue += event.target.innerHTML;
+        calculatorScreen.value = displayValue;
+        
     }
 });
 
+const posNeg = document.querySelector('.posNeg')
+posNeg.addEventListener('click', () => {
+    displayValue = displayValue * -1
+    calculatorScreen.value = displayValue
+    
+ });
+
+const percentButton = document.querySelector('.percentButton')
+percentButton.addEventListener('click', () => {
+    displayValue = displayValue/100
+    calculatorScreen.value = displayValue
+})
