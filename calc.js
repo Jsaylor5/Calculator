7795bccd58217eb7ce6a42db195c90d353b1781a
jsaylor5 <saylorj@live.com>
2021-11-03 15:28:44 -0400

    Added basic layout and buttons

---
+++ calc.js
@@ -0,0 +1,16 @@
+const add = (a,b) => a + b;
+
+const subtract = (a,b) => a - b;
+
+const multiply = (a,b) => a * b;
+
+const divide = (a,b) => a/b;
+
+const operate = (func, num1, num2) => func(num1, num2)
+
+let displayValue =[]
+
+const calculatorScreen = document.querySelector('#calculatorScreen')
+
+
+
